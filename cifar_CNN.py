33ba8027d8c969ab634da319e004ea5fbf003f56
fanch-luck <fch.luck@qq.com>
2021-08-05 14:08:54 +0800

    添加注释，减少空行。训练规模减少。

--- cifar_CNN.py
+++ cifar_CNN.py
@@ -11,10 +11,17 @@ import tensorflow as tf
 import pickle
 import matplotlib.pyplot as plt
 import warnings
+import os
+import time
 
 warnings.filterwarnings("ignore")
 # %matplotlib inline
 print("TensorFlow Version: %s" % tf.__version__)
+
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.60)
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 
 # 加载数据
@@ -29,11 +36,11 @@ def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
     # features and labels
     features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
     labels = batch['labels']
-    return features, labels
+    return features[:8000], labels[:8000]
 
 
 # 加载所有训练数据
-cifar10_path = 'R:/workspaces_files/tensor_resorce/demo_origin/batches' # 本地路径
+cifar10_path = 'R:/workspaces_files/tensor_resorce/demo_origin/batches'  # 本地路径
 # 共有5个batch的训练数据
 x_train, y_train = load_cfar10_batch(cifar10_path, 1)
 for i in range(2, 6):
@@ -48,6 +55,8 @@ with open(cifar10_path + '/test_batch', mode='rb') as file:
     x_test = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
     y_test = batch['labels']
 
+    x_test = x_test[:8000]
+    y_test = y_test[:8000]
 
 # 显示图片
 # fig, axes = plt.subplots(nrows=3, ncols=20, sharex=True, sharey=True, figsize=(80,12))
@@ -97,8 +106,8 @@ x_train_, x_val, y_train_, y_val = train_test_split(x_train,
 
 img_shape = x_train.shape
 keep_prob = 0.6
-epochs=5
-batch_size=64
+epochs=25
+batch_size=128
 
 
 inputs_ = tf.placeholder(tf.float32, [None, 32, 32, 3], name='inputs_')
@@ -106,25 +115,38 @@ targets_ = tf.placeholder(tf.float32, [None, n_class], name='targets_')
 
 # 第一层卷积加池化
 # 32 x 32 x 3 to 32 x 32 x 64
-conv1 = tf.layers.conv2d(inputs_, 64, (2,2), padding='same', activation=tf.nn.relu,
-                         kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+conv1 = tf.layers.conv2d(
+    inputs_,  #
+    64,  # 2D卷积窗口大小
+    (2, 2),
+    padding='same',
+    activation=tf.nn.tanh,
+    kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1)
+)
 # 32 x 32 x 64 to 16 x 16 x 64
-conv1 = tf.layers.max_pooling2d(conv1, (2,2), (2,2), padding='same')
+conv1 = tf.layers.max_pooling2d(
+    conv1,
+    (2, 2),
+    (2, 2),
+    padding='same'
+)
 
 # 第二层卷积加池化
 # 16 x 16 x 64 to 16 x 16 x 128
-conv2 = tf.layers.conv2d(conv1, 128, (4,4), padding='same', activation=tf.nn.relu,
-                         kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+conv2 = tf.layers.conv2d(
+    conv1, 128, (4, 4), padding='same', activation=tf.nn.relu,
+    kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
 # 16 x 16 x 128 to 8 x 8 x 128
 conv2 = tf.layers.max_pooling2d(conv2, (2,2), (2,2), padding='same')
 
 # 重塑输出
-shape = np.prod(conv2.get_shape().as_list()[1:])
-conv2 = tf.reshape(conv2,[-1, shape])
+shape = np.prod(conv2.get_shape().as_list()[1:])  # 获取conv2维度，转为列表取值
+conv2 = tf.reshape(conv2,[-1, shape])  # 按一维展开，-1：表示reshape时，该位置的大小自动获取，以便使总大小不变
 
 # 第一层全连接层
 # 8 x 8 x 128 to 1 x 1024
-fc1 = tf.contrib.layers.fully_connected(conv2, 1024, activation_fn=tf.nn.relu)
+fc1 = tf.contrib.layers.fully_connected(conv2, 1024, activation_fn=tf.nn.relu)  # 用于构建神经网络层、正则化器、摘要等的操作.
+
 fc1 = tf.nn.dropout(fc1, keep_prob)
 
 # 第二层全连接层
@@ -134,22 +156,23 @@ fc2 = tf.contrib.layers.fully_connected(fc1, 512, activation_fn=tf.nn.relu)
 # logits层
 # 1 x 512 to 1 x 10
 logits_ = tf.contrib.layers.fully_connected(fc2, 10, activation_fn=None)
-logits_ = tf.identity(logits_, name='logits_')
+logits_ = tf.identity(logits_, name='logits_')  # identify：输出与输入具有相同形状和内容的张量。将操作和名称关联
 
 # cost & optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits_, labels=targets_))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+# optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, epsilon=1e-5).minimize(cost)
 
 # accuracy
 correct_pred = tf.equal(tf.argmax(logits_, 1), tf.argmax(targets_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
 
 
-save_model_path = 'R:/workspaces_files/tensor_resorce/demo_origin/model/test_cifar'
+save_model_path = 'R:/workspaces_files/tensor_resorce/demo_origin/modelgpu/test_cifar'
 count = 0
+print('{}: start training'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-
     for epoch in range(epochs):
         for batch_i in range(img_shape[0] // batch_size - 1):
             feature_batch = x_train_[batch_i * batch_size: (batch_i + 1) * batch_size]
@@ -163,14 +186,16 @@ with tf.Session() as sess:
                                           targets_: y_val})
 
             if (count % 10 == 0):
-                print(
-                    'Epoch {:>2}, Train Loss {:.4f}, Validation Accuracy {:4f} '.format(epoch + 1, train_loss, val_acc))
-
+                print('{}: Epoch {:>2}, Train Loss {:.4f}, Validation Accuracy {:4f} '.format(
+                        time.strftime('%H:%M:%S', time.localtime()),
+                        epoch + 1,
+                        train_loss,
+                        val_acc))
             count += 1
-
     # 存储参数
     saver = tf.train.Saver()
     save_path = saver.save(sess, save_model_path)
+    print('{}: finishing training'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
 
 
 loaded_graph = tf.Graph()
@@ -190,6 +215,7 @@ with tf.Session(graph=loaded_graph) as sess:
     test_batch_acc_total = 0
     test_batch_count = 0
 
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
     print("Begin test...")
     for batch_i in range(x_test.shape[0] // test_batch_size - 1):
         test_feature_batch = x_test[batch_i * test_batch_size: (batch_i + 1) * test_batch_size]
@@ -198,5 +224,5 @@ with tf.Session(graph=loaded_graph) as sess:
             loaded_acc,
             feed_dict={loaded_x: test_feature_batch, loaded_y: test_label_batch})
         test_batch_count += 1
-
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
     print('Test Accuracy: {}\n'.format(test_batch_acc_total / test_batch_count))
